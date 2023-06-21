940c848524e07428c814388fc7b6ac7d8c484d75
clockwo <39350729+clockwo@users.noreply.github.com>
2023-06-21 22:43:29 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -8,10 +8,12 @@ module.exports = {
       ],
     }),
     require('postcss-nested'),
-    require('postcss-preset-env')(
-      { stage: 1 },
-      { features: { 'nesting-rules': false } }
-    ),
+    require('postcss-preset-env')({
+      stage: 1,
+      browsers: 'last 2 versions',
+      autoprefixer: { grid: true },
+      features: { 'nesting-rules': false },
+    }),
     require('postcss-reporter')({ clearReportedMessages: true }),
     require('cssnano'),
   ],
